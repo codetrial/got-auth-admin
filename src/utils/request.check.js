041be80f409d846a63b81c3824a4c812cec5967e
felixpy <felixpy.1993@gmail.com>
2019-01-02 18:30:56 +0800

    refactor: mock api and request util

--- src/utils/request.check.js
+++ src/utils/request.check.js
@@ -1,11 +1,11 @@
 const STATUS = {
-  OK: '200',
+  OK: ['200', 'OK'],
 };
 
 export function isOK({ status }) {
-  return status == null || status === STATUS.OK;
+  return status == null || STATUS.OK.indexOf(String(status)) >= 0;
 }
 
 export function isNotOK({ status }) {
-  return status != null && status !== STATUS.OK;
+  return status != null && STATUS.OK.indexOf(String(status)) < 0;
 }
